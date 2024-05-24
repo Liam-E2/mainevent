3c5b5146fa7c02608cb554a8df582b80ba21edea
Liam <liamearley7@protonmail.com>
2024-05-24 19:25:44 -0400

    Update demo

--- main.go
+++ main.go
@@ -7,6 +7,18 @@ import (
 	"os"
 )
 
+// Here we don't parameterize the demo poller with anything
+// But one could use Struct fields to parameterize custom pollers
+type DemoPoller struct {
+}
+
+func (p DemoPoller) Poll(opts eventsource.PollerConfig) ([]byte, error){
+	// Returns JSON-encoded bytes
+	data := []byte("{\"static_example\": \"of polling data...\"}")
+	return data, nil
+}
+
+
 func main() {
 	// Get addr from env
 	host, specified := os.LookupEnv("EVENTSOURCEHOST")
@@ -19,6 +31,19 @@ func main() {
 	}
 	addr := fmt.Sprintf("%s:%s", host, port)
 
+	// Demo Poller Setup
+	doneChan := make(chan bool)
+	pollConfs := eventsource.PollerConfig{
+		PollSeconds: 3, 
+		DoneChan: doneChan, 
+		EventName: "stream", 
+		EventServerAddr: "http://"+addr}
+
+	poller := DemoPoller{}
+
+	go eventsource.Run(poller, pollConfs) // Run poller in background
+	defer func(){doneChan <- true}() // defer turning off the poller
+	
 	// Run server
 	log.Fatal(eventsource.RunServer(addr))
 }
